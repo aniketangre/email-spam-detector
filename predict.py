5de63d6926bb577a3a94388fbb8b9dc1a4da5564
aniketangre <aniketangre.edu@gmail.com>
2025-05-09 22:34:21 +0200

    updated predict

--- predict.py
+++ predict.py
@@ -1,24 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
 import joblib
 import sys
-
-
-# In[10]:
-
 
 def load_model():
     model = joblib.load('spam_detector_model.pkl')
     vectorizor = joblib.load('spam_detector_vectorizor.pkl')
     return model, vectorizor
-
-
-# In[11]:
-
 
 def predict(text):
     model, vectorizor = load_model()
@@ -26,17 +12,10 @@ def predict(text):
     prediction = model.predict(text_transformed)
     return "spam" if prediction[0] == 1 else "Not Spam"
 
-
-# In[13]:
-
-
 if __name__ == "__main__":
     text = sys.argv[1] #"Hello world!"
     prediction = predict(text)
     print(f"The message is: {prediction}")
-
-
-# In[ ]:
 
 
 
